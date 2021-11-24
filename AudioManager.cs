6df69fb2a93a9ae54532c77b54f15ebcc88ff985
M3lodias <74119372+M3lodias@users.noreply.github.com>
2021-11-24 12:30:52 +0100

    Supprimer une ligne de code d'audioManager
    Cette ligne faisait persister le manager dans le menu.

---
+++ AudioManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    
+    [HideInInspector]
+    public Sound CurrentSound;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.mixer;
+        }
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        CurrentSound = s;
+        if (s == null)
+        {
+            Debug.LogWarning("Sound : " + name + " not found");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Play(int index)
+    {
+        Sound s = sounds[index];
+        CurrentSound = s;
+        if (s == null)
+        {
+            Debug.LogWarning("Sound : " + name + " not found");
+            return;
+        }
+        s.source.Play();
+    }
+}
