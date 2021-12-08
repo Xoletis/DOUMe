ad16aa8b1d783132da2bf9ef574ade581a35b732
Mega2nium <58862901+Mega2nium@users.noreply.github.com>
2021-12-08 11:16:38 +0100

    Sounds (part of)
    Archives de tout les sons implémentés dans notre jeu + tous les sons trouvés (mais pas forcément mis dans notre jeu)
    Ajout des sons des armes en ajoutant des détails dans les scriptes AudioManager, Weapon stats et gun controller

---
+++ GunController.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    //La caméra
+    private Camera fpsCam;
+
+    //Float : mémorise le temps du prochain tir possible
+    private float nextFire;
+
+    //Détermine sur quel Layer on peut tirer
+    public LayerMask layerMask;
+
+    private PlayerInventory inventory;
+    [SerializeField]
+    private WeaponStats weapon;
+
+    public string enemieDamageFunction;
+
+    public UnityEngine.UI.Image weaponImage;
+    public UnityEngine.UI.Text ammoText;
+
+    bool canFire = true;
+
+    private AudioManager audioManager;
+
+    void Start()
+    {
+        fpsCam = GetComponentInChildren<Camera>();
+        inventory = GetComponent<PlayerInventory>();
+
+        audioManager = FindObjectOfType<AudioManager>();
+
+        weapon = inventory.GetWeapon();
+        weaponImage.sprite = weapon.Image;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammoText.text = weapon.munitions + "";
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadAmmo();
+        }
+
+        
+        if (Input.GetButton("Fire1") && Time.time > nextFire && weapon.munitions == 0)
+        {
+            Debug.Log("Pas de munitions");
+
+        }
+
+        // Vérifie si le joueur a pressé le bouton pour faire feu (bouton gauche de la souris)
+        // Time.time > nextFire : vérifie si suffisament de temps s'est écoulé pour pouvoir tirer à nouveau
+        if (Input.GetButton("Fire1") && Time.time > nextFire && weapon.munitions > 0 && canFire) {
+           
+            audioManager.Play(weapon.firingSound);
+            weapon.munitions--;
+
+            //Met à jour le temps pour le prochain tir
+            //Time.time = Temps écoulé depuis le lancement du jeu
+            //temps du prochain tir = temps total écoulé + temps qu'il faut attendre
+            nextFire = Time.time + weapon.fireRate;
+
+            //On va lancer un rayon invisible qui simulera les balles du gun
+
+            //Crée un vecteur au centre de la vue de la caméra
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+            //RaycastHit : permet de savoir ce que le rayon a touché
+            RaycastHit hit;
+
+
+            // Vérifie si le raycast a touché quelque chose
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weapon.wpnRange, layerMask))
+            {
+                // Vérifie si la cible a un RigidBody attaché
+                if (hit.rigidbody != null)
+                {
+                    //S'assure que la cible touchée a un composant ReceiveAction
+                    hit.collider.gameObject.SendMessage(enemieDamageFunction, weapon.wpnDmg);
+                }
+            }
+        }
+    }
+
+    private void ReloadAmmo()
+    {
+        if (inventory.GetMunition() > 0 && weapon.munitions != weapon.maxMunitions)
+        {
+            StartCoroutine(reloadTime());
+        }
+        else
+        {
+            Debug.Log("Echec du rechargement de munitions");
+        }
+    }
+
+    public void changeWeapon()
+    {
+        weapon = inventory.GetWeapon();
+        weaponImage.sprite = weapon.Image;
+    }
+
+    IEnumerator reloadTime()
+    {
+        canFire = false;
+        weaponImage.enabled = false;
+        audioManager.Play(weapon.reloadSound);
+        yield return new WaitForSeconds(weapon.reaoldTime);
+        int munitionARajouter = weapon.maxMunitions - weapon.munitions;
+        if (inventory.GetMunition() < weapon.maxMunitions)
+        {
+            weapon.munitions = inventory.GetMunition();
+            inventory.AddMunition(-inventory.GetMunition());
+        }
+        else
+        {
+            weapon.munitions = weapon.maxMunitions;
+            inventory.AddMunition(-munitionARajouter);
+        }
+        Debug.Log("munitions remplies !");
+        canFire = true;
+        weaponImage.enabled = true;
+    }
+}
---
+++ WeaponStats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "My Game/Weapon")]
+public class WeaponStats : ScriptableObject
+{
+    public enum AmmoType { shotgun, gun, chaingun }
+
+    //attributes
+    public int wpnDmg = 1;
+    public float fireRate = 0.25f;
+    public float wpnRange = 200f;
+    public int munitions = 0;
+    public int maxMunitions = 50;
+    public float reaoldTime = 2f;
+    public Sprite Image;
+    public AmmoType ammoType;
+
+    public string firingSound;
+    public string reloadSound;
+}
